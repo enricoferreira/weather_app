134c7fba1714274de4bb9f6005a93af7331c373c
Enrico Ferreira <enricoferreira41@gmail.com>
2022-06-21 18:21:23 -0300

    instalando o apex e configurando no main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import VueApexCharts from "vue3-apexcharts";
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+createApp(App).use(router).use(store).use(vuetify).use(VueApexCharts).mount("#app");
